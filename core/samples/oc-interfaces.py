665b5ee499b9bf5eed98ad6b3bca087551b3ee18
Santiago Alvarez <saalvare@cisco.com>
2016-09-07 16:23:43 -0700

    Make oc-interfaces sample executable

--- core/samples/oc-interfaces.py
+++ core/samples/oc-interfaces.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #  ----------------------------------------------------------------
 # Copyright 2016 Cisco Systems
 #
@@ -27,7 +28,7 @@ from ydk.types import Empty, DELETE, Decimal64
 from ydk.services import CRUDService
 import logging
 
-from samples.session_mgr import establish_session, init_logging
+from session_mgr import establish_session, init_logging
 from ydk.models.openconfig.openconfig_interfaces import Interfaces
 from ydk.errors import YPYError
 
