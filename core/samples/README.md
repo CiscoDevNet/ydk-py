10fdde925510e4bf357f357715957dfdaa7754f3
Abhi Keshav <abhirame@cisco.com>
2017-03-08 13:48:00 -0800

    Add YDK app maker

---
+++ core/samples/README.md
@@ -0,0 +1,15 @@
+# Samples 
+
+To run the samples, install the ydk [core](https://github.com/CiscoDevNet/ydk-py/tree/master/core) and any bundles (ietf, openconfig, cisco-ios-xr) as desired.
+
+## To run:
+
+For example, to run the `bgp.py` sample:
+```
+$ ./bgp.py --host <ip-address-of-netconf-server> -u <username> -p <password> --port <port-number>
+```
+
+YAM stands for YDK app maker. This provides an easy way to generate YDK code snippets from XML configs. To run `yam_sample.py`:
+```
+$ ./yam_sample.py
+```
--- core/ydk/providers/_encoder.py
+++ core/ydk/providers/_encoder.py
@@ -32,10 +32,15 @@ from ._importer import _yang_ns
 class XmlEncoder(object):
     def __init__(self):
         self.encode_value = ValueEncoder().encode
+        self.app=''
+        self.python_imports = {}
 
     def encode(self, entity):
         ''' Convert the entity to an xml payload '''
-        return etree.tostring(self.encode_to_xml(entity, etree.Element('a'), ''), method='xml', pretty_print='True', encoding='utf-8').decode('utf-8')
+        ret = etree.tostring(self.encode_to_xml(entity, etree.Element('a'), ''), method='xml', pretty_print='True', encoding='utf-8').decode('utf-8')
+        self._add_imports()
+        self.app = _enclose_function_definition(entity, self.app)
+        return ret
 
     def encode_to_xml(self, entity, root, optype, is_filter=False):
         ''' Convert the entity to an xml payload '''
@@ -51,6 +56,9 @@ class XmlEncoder(object):
             elem = root
         else:
             elem = etree.SubElement(root, entity.i_meta.yang_name)
+            self._store_import(entity.i_meta.pmodule_name)
+            self.app += _get_object_declaration_statement(entity)
+
             parent_ns = None
             current_parent = root
             while current_parent != None and parent_ns is None:
@@ -88,10 +96,12 @@ class XmlEncoder(object):
                 continue
             elif member.mtype == REFERENCE_CLASS:
                 self.encode_to_xml(value, elem, optype)
+                self.app += _get_member_assignment_statement(entity, member)
             elif member.mtype == REFERENCE_LIST:
                 child_list = value
                 for child in child_list:
                     self.encode_to_xml(child, elem, optype)
+                    self.app += _get_member_append_statement(entity, member)
             elif member.mtype == REFERENCE_LEAFLIST and isinstance(value, list):
                 for child in value:
 
@@ -100,6 +110,7 @@ class XmlEncoder(object):
                     text = self.encode_value(member, NSMAP, child.item)
                     member_elem = etree.SubElement(elem, member.name, nsmap=NSMAP)
                     member_elem.text = text
+                    self.app += _get_value_append_statement(entity, member, text)
             elif member.mtype == REFERENCE_UNION:
                 for contained_member in member.members:
                     NSMAP={}
@@ -109,12 +120,18 @@ class XmlEncoder(object):
                 text = self.encode_value(member, NSMAP, value)
                 member_elem = etree.SubElement(elem, member.name, nsmap=NSMAP)
                 member_elem.text = text
+                self._store_import(value._meta_info().pmodule_name)
+                self.app += _get_value_assignment_statement(entity, member, value)
             else:
-                member_elem.text = self.encode_value(member, NSMAP, value)
+                if member.mtype == REFERENCE_ENUM_CLASS:
+                    self._store_import(value._meta_info().pmodule_name)
+                text = self.encode_value(member, NSMAP, value)
+                member_elem.text = text
+                self.app += _get_value_assignment_statement(entity, member, value)
         return elem
 
-    def encode_filter(self, filter, root, optype):
-        self.encode_to_xml(filter, root, optype, True)
+    def encode_filter(self, f, root, optype):
+        self.encode_to_xml(f, root , optype, True)
 
     def _encode_union_member(self, entity, contained_member, value, elem, NSMAP):
         member_elem = None
@@ -127,6 +144,7 @@ class XmlEncoder(object):
                 t = self.encode_value(contained_member, NSMAP, child.item)
                 if len(t) == 0:
                     continue
+                self.app += _get_value_append_statement(entity, contained_member, t)
                 member_elem = etree.SubElement(elem, contained_member.name, nsmap=NSMAP)
                 if entity.i_meta.namespace is not None and entity.i_meta.namespace != _yang_ns._namespaces[contained_member.module_name]:
                     NSMAP[None] = _yang_ns._namespaces[contained_member.module_name]
@@ -138,10 +156,28 @@ class XmlEncoder(object):
             if len(t) == 0:
                     return
             member_elem = etree.SubElement(elem, contained_member.name, nsmap=NSMAP)
+            if contained_member.mtype == REFERENCE_ENUM_CLASS:
+                self._store_import(value._meta_info().pmodule_name)
+            self.app += _get_value_assignment_statement(entity, contained_member, value)
             member_elem.text = t
             if len(member_elem.text) > 0:
                 return True
         return False
+
+    def _add_imports(self):
+        import_statements = ''
+        for import_statement in self.python_imports.keys():
+            import_statements += import_statement + '\n'
+        import_statements += '    from ydk.types import Empty\n\n'
+        self.app = import_statements + self.app
+
+    def _store_import(self, pmodule_name):
+        import_from = '.'.join(pmodule_name.split('.')[:-1])
+        import_module = pmodule_name.split('.')[-1]
+        import_statement = '    from ' + import_from + ' import ' + import_module
+        if import_statement in self.python_imports:
+            return
+        self.python_imports[import_statement] = True
 
 
 def entity_is_rpc_input(entity):
@@ -150,3 +186,67 @@ def entity_is_rpc_input(entity):
         hasattr(entity.parent, 'is_rpc') and \
         entity.parent.is_rpc and \
         entity._meta_info().yang_name == 'input'
+
+
+def _enclose_function_definition(entity, app):
+    return '\ndef _create_' + _dash_to_underscore(entity.i_meta.yang_name) + '_ydk_object():\n' + \
+        app + '\n    return ' + _dash_to_underscore(entity.i_meta.yang_name) + '\n'
+
+
+def _get_object_declaration_statement(entity):
+    return '\n    ' + _dash_to_underscore(entity.i_meta.yang_name) + ' = ' + _get_module_name(entity.i_meta.pmodule_name) + '.' + entity.i_meta.name + '()\n'
+
+
+def _get_value_assignment_statement(entity, member, value):
+    if member.ptype == 'Empty':
+        return '    ' + _dash_to_underscore(entity.i_meta.yang_name) + '.' + member.presentation_name + ' = Empty()' + '\n'
+    return '    ' + _dash_to_underscore(entity.i_meta.yang_name) + '.' + member.presentation_name + ' = ' + _get_encoded_value(member, value) + '\n'
+
+
+def _get_value_append_statement(entity, member, value):
+    return '    ' + _dash_to_underscore(entity.i_meta.yang_name) + '.' + member.presentation_name + '.append(' + _get_encoded_value(member, value) + ')\n'
+
+
+def _get_member_assignment_statement(entity, member):
+    return '    ' + _dash_to_underscore(entity.i_meta.yang_name) + '.' + member.presentation_name + ' = ' + '' + member.presentation_name + '\n'
+
+
+def _get_member_append_statement(entity, member):
+    return '    ' + _dash_to_underscore(entity.i_meta.yang_name) + '.' + member.presentation_name + '.append(' + '' + member.presentation_name + ')\n'
+
+
+def _get_encoded_value(member, value):
+    if member.mtype == REFERENCE_ENUM_CLASS:
+        value = _dash_to_underscore(value._meta_info().module_name) + _get_class_name(member) + '.' + str(value)
+        return value
+    elif member.mtype == REFERENCE_IDENTITY_CLASS:
+        value = _dash_to_underscore(value._meta_info().module_name) + _get_class_name(member) + '.' + value.__class__.__name__ + '()'
+        return value
+    else:
+        return _escape_value(value)
+
+
+def _get_class_name(member):
+    clazz_name = '.'.join(member.clazz_name.split('.')[:-1])
+    if clazz_name == '':
+        clazz_name = ''
+    else:
+        clazz_name = '.' + clazz_name
+    return clazz_name
+
+
+def _dash_to_underscore(name):
+    import keyword
+    if keyword.iskeyword(name) or name in ('None', 'parent'):
+        return name.replace('-', '_') + '_'
+    return name.replace('-', '_')
+
+
+def _escape_value(value):
+    if isinstance(value, str):
+        return "'" + str(value) + "'"
+    return str(value)
+
+
+def _get_module_name(full_name):
+    return full_name.split('.')[-1]
