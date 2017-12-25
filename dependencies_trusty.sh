e7f79b5d5e0c21716573cd7736ab734ba8517fa2
Abhi Keshav <abhirame@cisco.com>
2017-12-25 14:35:50 -0800

    Add testing for libydk+ydk-py

---
+++ dependencies_trusty.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get update > /dev/null
+sudo apt-get install libssh-dev libtool gdebi-core python3-dev python-dev -y > /dev/null
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y > /dev/null
+sudo apt-get update > /dev/null
+sudo apt-get install cmake gcc-5 g++-5 -y > /dev/null
+sudo ln -f -s /usr/bin/g++-5 /usr/bin/c++
+sudo ln -f -s /usr/bin/gcc-5 /usr/bin/cc
+git clone https://github.com/ciscodevnet/ydk-cpp.git -b 0.6.3
+mkdir ydk-cpp/core/ydk/build
+cd ydk-cpp/core/ydk/build
+cmake -DCMAKE_BUILD_TYPE=Release .. 
+sudo make install
+cd -
+sudo easy_install pip
---
+++ tests.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function pip_check_install {
+    if [[ $(uname) == "Linux" ]] ; then
+        os_info=$(cat /etc/*-release)
+        if [[ ${os_info} == *"fedora"* ]]; then
+            echo "custom pip install of $@ for centos"
+            pip install --install-option="--install-purelib=/usr/lib64/python2.7/site-packages" --no-deps $@
+            return
+        fi
+    elif [[ $(uname) == "Darwin" ]] ; then
+        sudo pip install $@
+        return
+    fi
+    pip install $@
+}
+
+cd core
+python setup.py sdist
+pip install  dist/*.tar.gz
+cd ../ietf
+python setup.py  sdist
+pip_check_install  dist/*.tar.gz
+cd ../openconfig
+python setup.py sdist
+pip_check_install  dist/*.tar.gz
+cd ../cisco-ios-xr
+python setup.py sdist
+pip_check_install  dist/*.tar.gz
+cd ../cisco-ios-xe
+python setup.py sdist
+pip_check_install  dist/*.tar.gz
+
+echo "Running codec sample"
+cd ../core/samples
+./bgp_codec.py
