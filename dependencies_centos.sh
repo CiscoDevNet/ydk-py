59b2169af7f3b1f53b7d84d8c3eafd901a864bd8
Abhi Keshav <abhirame@cisco.com>
2018-01-02 17:59:09 -0500

    Add testing for libydk+ydk-py (#32)

---
+++ dependencies_centos.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+yum install -y epel-release
+yum install -y libxslt-devel libssh-devel python-devel cmake3 python-pip which make sudo > /dev/null
+yum install -y https://devhub.cisco.com/artifactory/rpm-ydk/0.6.3/libydk-0.6.3-1.x86_64.rpm > /dev/null
+
+yum install centos-release-scl -y > /dev/null
+yum install devtoolset-4-gcc* -y > /dev/null
+
+ln -sf /opt/rh/devtoolset-4/root/usr/bin/gcc /usr/bin/cc
+ln -sf /opt/rh/devtoolset-4/root/usr/bin/g++ /usr/bin/c++
+
+sudo easy_install pip
---
+++ dependencies_osx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+brew update > /dev/null
+brew install libssh xml2 curl pybind11 > /dev/null
+
+brew rm -f --ignore-dependencies python python3
+
+curl -O https://devhub.cisco.com/artifactory/osx-ydk/0.6.3/libydk-0.6.3-Darwin.pkg
+sudo installer -pkg libydk*pkg -target /
+
+sudo easy_install pip
