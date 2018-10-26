849304a40e15b9b764fc88abb792c00838f855ce
Yan Gorelik <yangorelik@Yans-iMac.local>
2018-10-25 22:01:08 -0700

    Fixing Travis run for OSX

--- dependencies_trusty.sh
+++ dependencies_trusty.sh
@@ -9,13 +9,11 @@ NOCOLOR="\033[0m"
 YELLOW='\033[1;33m'
 MSG_COLOR=$YELLOW
 
-print_msg "Installing Linux dependencies"
-sudo apt-get update > /dev/null
-sudo apt-get install libssh-dev libtool gdebi-core python3-dev python-dev -y > /dev/null
+print_msg "Installing Trusty OS dependencies"
+sudo apt-get update && apt-get install -y --no-install-recommends apt-utils > /dev/null
+sudo apt-get install libssh-dev libtool gdebi-core python3-dev python-dev wget cmake3 -y > /dev/null
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y > /dev/null
 sudo apt-get update > /dev/null
-sudo easy_install pip
-sudo pip install pybind11
 
 print_msg "Installing C++ version 5"
 sudo apt-get install cmake gcc-5 g++-5 -y > /dev/null
@@ -26,6 +24,14 @@ print_msg "Installing YDK C++ core library"
 git clone https://github.com/ciscodevnet/ydk-cpp.git -b 0.7.3
 mkdir ydk-cpp/core/ydk/build
 cd ydk-cpp/core/ydk/build
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake3 -DCMAKE_BUILD_TYPE=Release ..
 sudo make install
 cd -
+
+pip -V &> /dev/null
+status=$?
+if [ $status -ne 0 ]; then
+  print_msg "Installing pip"
+  curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+  python get-pip.py
+fi
