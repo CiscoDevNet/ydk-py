5fa966f8a7369f70b77f86bdb67b1512e6ba3b7a
Yan <ygorelik@cisco.com>
2018-12-04 06:31:12 -0800

    Fixing Travis test for OSX

--- dependencies_osx.sh
+++ dependencies_osx.sh
@@ -5,23 +5,22 @@ function print_msg {
 }
 
 function install_os_dependencies {
-    brew install curl doxygen xml2 pybind11
+    brew install curl xml2 doxygen pybind11
+    brew rm -f --ignore-dependencies python python3
 }
 
 function install_libssh {
     print_msg "Installing libssh-0.7.6"
     brew reinstall openssl
     export OPENSSL_ROOT_DIR=/usr/local/opt/openssl
-
-#    wget https://devhub.cisco.com/artifactory/osx-ydk/third-party/libssh-0.7.5.pkg
-#    sudo installer -pkg libssh-0.7.5.pkg target /
-
     wget https://git.libssh.org/projects/libssh.git/snapshot/libssh-0.7.6.tar.gz
     tar zxf libssh-0.7.6.tar.gz && rm -f libssh-0.7.6.tar.gz
     mkdir libssh-0.7.6/build && cd libssh-0.7.6/build
     cmake ..
     sudo make install
     cd -
+#    wget https://devhub.cisco.com/artifactory/osx-ydk/third-party/libssh-0.7.5.pkg
+#    sudo installer -pkg libssh-0.7.5.pkg -target /
 }
 
 function install_libydk {
